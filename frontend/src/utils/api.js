84d709a80271916707cda4a3931d6b4facb8761e
Sravanam Rakesh Kumar <sravanamrakeshkumar3@gmail.com>
2025-07-16 20:37:23 +0530

    final2 commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${backendUrl}/api`,
   headers: {
     'Content-Type': 'application/json'
   }
